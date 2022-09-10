f471e93a84bf52b2e5fe786b6710f357bee54cde
SergeyAnshin <anshin.serega@gmail.com>
2022-09-10 20:17:30 +0300

    init structure

---
+++ src/main/java/com/mars/ansh/projectteamsearcher/ProjectTeamSearcherApplication.java
@@ -0,0 +1,13 @@
+package com.mars.ansh.projectteamsearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTeamSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTeamSearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mars/ansh/projectteamsearcher/ProjectTeamSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mars.ansh.projectteamsearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTeamSearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
