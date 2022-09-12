70068db05e35922524e605fb7d131ccc853086d1
SergeyAnshin <anshin.serega@gmail.com>
2022-09-12 15:28:21 +0300

    create Project entity

---
+++ src/main/java/com/mars/ansh/projectteamsearcher/entity/Project.java
@@ -0,0 +1,22 @@
+package com.mars.ansh.projectteamsearcher.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+public class Project extends GeneralEntity {
+    private String name;
+    private String description;
+    private String link;
+}
