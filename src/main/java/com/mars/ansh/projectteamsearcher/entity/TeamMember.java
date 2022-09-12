e7ea74414fe982ae40b362e3fcdf61479017145d
SergeyAnshin <anshin.serega@gmail.com>
2022-09-12 14:54:33 +0300

    create TeamMember entity

---
+++ src/main/java/com/mars/ansh/projectteamsearcher/entity/TeamMember.java
@@ -0,0 +1,18 @@
+package com.mars.ansh.projectteamsearcher.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+public class TeamMember extends GeneralEntity {
+}
