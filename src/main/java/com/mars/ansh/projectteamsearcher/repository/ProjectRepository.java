04378a74ece9298fc10562c678c86fe80d50d646
SergeyAnshin <anshin.serega@gmail.com>
2022-09-12 21:26:31 +0300

    create ProjectRepository

---
+++ src/main/java/com/mars/ansh/projectteamsearcher/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.mars.ansh.projectteamsearcher.repository;
+
+import com.mars.ansh.projectteamsearcher.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
