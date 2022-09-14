00cabe11f31ef7cf7203e43793d6938972010ae9
SergeyAnshin <anshin.serega@gmail.com>
2022-09-14 16:03:52 +0300

    create TechnologyRepository

---
+++ src/main/java/com/mars/ansh/projectteamsearcher/repository/TechnologyRepository.java
@@ -0,0 +1,7 @@
+package com.mars.ansh.projectteamsearcher.repository;
+
+import com.mars.ansh.projectteamsearcher.entity.Technology;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TechnologyRepository extends JpaRepository<Technology, Long> {
+}
