59a5bc6c10d932cb02a064b3ebfa69f94ac7bacb
SergeyAnshin <anshin.serega@gmail.com>
2022-09-17 15:23:30 +0300

    add save method in ProjectController

--- src/main/java/com/mars/ansh/projectteamsearcher/controller/ProjectController.java
+++ src/main/java/com/mars/ansh/projectteamsearcher/controller/ProjectController.java
@@ -1,13 +1,25 @@
 package com.mars.ansh.projectteamsearcher.controller;
 
+import com.mars.ansh.projectteamsearcher.dto.ProjectDto;
+import com.mars.ansh.projectteamsearcher.entity.Project;
 import com.mars.ansh.projectteamsearcher.service.ProjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/projects")
 public class ProjectController {
+    private static final String GENERAL_ENDPOINT_FOR_PROJECT = "/projects";
     private final ProjectService projectService;
+
+    @PostMapping
+    public ResponseEntity<Object> save(@RequestBody ProjectDto projectDto) {
+        Project savedProject = projectService.save(projectDto);
+        return ResponseEntity.created(URI.create(String.join("/", GENERAL_ENDPOINT_FOR_PROJECT,
+                String.valueOf(savedProject.getId())))).build();
+    }
 }
