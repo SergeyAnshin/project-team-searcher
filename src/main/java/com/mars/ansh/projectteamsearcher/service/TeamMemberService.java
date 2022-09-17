40b313d8c55077afa083baae48db26e5753a1ffb
SergeyAnshin <anshin.serega@gmail.com>
2022-09-17 15:18:43 +0300

    add findAllByPositionNames method in TeamMemberService

--- src/main/java/com/mars/ansh/projectteamsearcher/service/TeamMemberService.java
+++ src/main/java/com/mars/ansh/projectteamsearcher/service/TeamMemberService.java
@@ -1,11 +1,18 @@
 package com.mars.ansh.projectteamsearcher.service;
 
+import com.mars.ansh.projectteamsearcher.entity.TeamMember;
 import com.mars.ansh.projectteamsearcher.repository.TeamMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class TeamMemberService {
     private final TeamMemberRepository teamMemberRepository;
+
+    public Set<TeamMember> findAllByPositionNames(Set<String> positionNames) {
+        return teamMemberRepository.findAllByPositionNameIn(positionNames);
+    }
 }
