bc8e79a0b23c8674d9fdac7326a0c969246e354b
SergeyAnshin <anshin.serega@gmail.com>
2022-09-17 15:17:48 +0300

    add findAllByNames method in TechnologyService

--- src/main/java/com/mars/ansh/projectteamsearcher/service/TechnologyService.java
+++ src/main/java/com/mars/ansh/projectteamsearcher/service/TechnologyService.java
@@ -1,11 +1,19 @@
 package com.mars.ansh.projectteamsearcher.service;
 
+import com.mars.ansh.projectteamsearcher.entity.Technology;
+import com.mars.ansh.projectteamsearcher.exception.EntityNotExistsException;
 import com.mars.ansh.projectteamsearcher.repository.TechnologyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class TechnologyService {
     private final TechnologyRepository technologyRepository;
+
+    public Set<Technology> findAllByNames(Set<String> names) {
+        return technologyRepository.findAllByNameIn(names);
+    }
 }
