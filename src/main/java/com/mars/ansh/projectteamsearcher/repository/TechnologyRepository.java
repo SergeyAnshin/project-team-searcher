9750775f1e0c295574f6291ceb976948d2dcf45d
SergeyAnshin <anshin.serega@gmail.com>
2022-09-17 15:16:30 +0300

    add findAllByNameIn method in TechnologyRepository

--- src/main/java/com/mars/ansh/projectteamsearcher/repository/TechnologyRepository.java
+++ src/main/java/com/mars/ansh/projectteamsearcher/repository/TechnologyRepository.java
@@ -3,5 +3,9 @@ package com.mars.ansh.projectteamsearcher.repository;
 import com.mars.ansh.projectteamsearcher.entity.Technology;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface TechnologyRepository extends JpaRepository<Technology, Long> {
+
+    Set<Technology> findAllByNameIn(Set<String> names);
 }
