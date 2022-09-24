c6adca95cc965fc8cfd74a18bcbf320fd4ae513d
SergeyAnshin <anshin.serega@gmail.com>
2022-09-24 18:25:12 +0300

    add parameters to EntityAlreadyExistsException in ProjectService

--- src/main/java/com/mars/ansh/projectteamsearcher/service/ProjectService.java
+++ src/main/java/com/mars/ansh/projectteamsearcher/service/ProjectService.java
@@ -3,30 +3,31 @@ package com.mars.ansh.projectteamsearcher.service;
 import com.mars.ansh.projectteamsearcher.dto.ProjectDto;
 import com.mars.ansh.projectteamsearcher.entity.Project;
 import com.mars.ansh.projectteamsearcher.entity.TeamMember;
-import com.mars.ansh.projectteamsearcher.entity.Technology;
 import com.mars.ansh.projectteamsearcher.exception.EntityAlreadyExistsException;
 import com.mars.ansh.projectteamsearcher.mapper.ProjectMapper;
 import com.mars.ansh.projectteamsearcher.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
 public class ProjectService {
-    private static final String PROJECT_EXISTS_ERROR_MESSAGE_CODE = "error.exists.project";
+    private static final String ENTITY_EXISTS_MESSAGE_CODE = "exception.entity.exists.message";
+    private static final String ENTITY_NAME_PROJECT = "entity.name.project";
     private final ProjectRepository projectRepository;
     private final TechnologyService technologyService;
-    private final TeamMemberService teamMemberService;
     private final PositionService positionService;
     private final ProjectMapper projectMapper;
+    private final MessageSource messageSource;
 
     public Project save(ProjectDto projectDto) throws EntityAlreadyExistsException {
         Project project = projectMapper.projectDtoToProject(projectDto);
         if (projectRepository.existsByLink(project.getLink())) {
-            throw new EntityAlreadyExistsException(PROJECT_EXISTS_ERROR_MESSAGE_CODE);
+            throw new EntityAlreadyExistsException(ENTITY_EXISTS_MESSAGE_CODE,
+                    messageSource.getMessage(ENTITY_NAME_PROJECT, null, Locale.getDefault()));
         }
         technologyService.findAllByNames(projectDto.getTechnologies())
                 .forEach(project::addTechnology);
