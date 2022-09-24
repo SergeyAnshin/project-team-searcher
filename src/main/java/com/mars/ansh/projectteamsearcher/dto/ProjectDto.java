2846e622946d3d98496f3ef16956bcafe064b41a
SergeyAnshin <anshin.serega@gmail.com>
2022-09-24 19:23:12 +0300

    add validation constraints to ProjectDto

--- src/main/java/com/mars/ansh/projectteamsearcher/dto/ProjectDto.java
+++ src/main/java/com/mars/ansh/projectteamsearcher/dto/ProjectDto.java
@@ -1,18 +1,23 @@
 package com.mars.ansh.projectteamsearcher.dto;
 
+import com.mars.ansh.projectteamsearcher.validator.constraint.ExistingPosition;
+import com.mars.ansh.projectteamsearcher.validator.constraint.ExistingTechnology;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import java.util.Set;
 
 @Builder
 @Getter
 @Setter
 public class ProjectDto {
+    @NotBlank
     private String name;
     private String description;
+    @NotBlank
     private String link;
-    private Set<String> technologies;
-    private Set<String> requiredPositions;
+    private Set<@ExistingTechnology String> technologies;
+    private Set<@ExistingPosition String> requiredPositions;
 }
