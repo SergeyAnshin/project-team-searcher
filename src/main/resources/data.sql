b26515eb054d0a3b58f609f6ce72d1c4f1e5676b
SergeyAnshin <anshin.serega@gmail.com>
2022-09-26 00:06:25 +0300

    delete data.sql file

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO technology(creation_date_time, update_date_time, name) VALUES (NOW(), NOW(), 'Java'),
-    (NOW(), NOW(), 'JS');
-INSERT INTO position(creation_date_time, update_date_time,name) VALUES (NOW(), NOW(), 'AQA'),
-    (NOW(), NOW(), 'Back-end'), (NOW(), NOW(), 'PM');
