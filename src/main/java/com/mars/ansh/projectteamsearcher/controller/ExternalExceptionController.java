5bd60c51e7ec163b952235779863f18b9793f5c6
SergeyAnshin <anshin.serega@gmail.com>
2022-09-30 22:33:27 +0300

    add handleConstraintViolationException method in ExternalExceptionController

--- src/main/java/com/mars/ansh/projectteamsearcher/controller/ExternalExceptionController.java
+++ src/main/java/com/mars/ansh/projectteamsearcher/controller/ExternalExceptionController.java
@@ -1,14 +1,19 @@
 package com.mars.ansh.projectteamsearcher.controller;
 
+import com.mars.ansh.projectteamsearcher.entity.ApiValidationError;
 import com.mars.ansh.projectteamsearcher.service.ApiErrorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -21,5 +26,11 @@ public class ExternalExceptionController extends ResponseEntityExceptionHandler 
                                                                   WebRequest request) {
         return ResponseEntity.badRequest()
                 .body(apiErrorService.generateMessage(ex, request.getLocale()));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<? super ApiValidationError>> handleConstraintViolationException(ConstraintViolationException exception) {
+        return ResponseEntity.badRequest()
+                .body(apiErrorService.generateMessage(exception));
     }
 }
