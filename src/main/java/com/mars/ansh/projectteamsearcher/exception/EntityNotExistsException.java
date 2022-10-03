121248c9597a1bddddcdddb0a552b2f5125b56c4
SergeyAnshin <anshin.serega@gmail.com>
2022-10-03 13:05:52 +0300

    add missingValue field in EntityNotExistsException

--- src/main/java/com/mars/ansh/projectteamsearcher/exception/EntityNotExistsException.java
+++ src/main/java/com/mars/ansh/projectteamsearcher/exception/EntityNotExistsException.java
@@ -1,12 +1,19 @@
 package com.mars.ansh.projectteamsearcher.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EntityNotExistsException extends BusinessException {
+    private String missingValue;
 
     public EntityNotExistsException() {
         super();
     }
 
-    public EntityNotExistsException(String messageSourceCode, String entityName) {
+    public EntityNotExistsException(String messageSourceCode, String entityName, String missingValue) {
         super(messageSourceCode, entityName);
+        this.missingValue = missingValue;
     }
 }
